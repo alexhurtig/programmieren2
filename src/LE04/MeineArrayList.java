0328b781c245318830d028f3b1428858b20534a2
alexhurtig <alexander.hurtig@friendsurance.de>
2017-10-27 11:38:27 +0200

    neues File für le04 und le05

---
+++ src/LE04/MeineArrayList.java
@@ -0,0 +1,17 @@
+package LE04;
+
+public class MeineArrayList<E> {
+
+    private E[] list;
+    private int size;
+    private int capacity;
+    private int increment;
+
+// with initial capacity c
+    public MeineArrayList(int c, int inc) {
+        capacity = c;
+        list = (E[]) new Object[c];
+        size = 0;
+        increment = inc;
+    }
+}
