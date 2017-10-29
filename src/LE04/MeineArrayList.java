28ad04fdd564262b32b2ec352f3ee504079dbb5e
alexhurtig <alexander.hurtig@friendsurance.de>
2017-10-29 23:12:16 +0100

    neues File für le04 und le05

--- src/LE04/MeineArrayList.java
+++ src/LE04/MeineArrayList.java
@@ -1,5 +1,7 @@
 package LE04;
 
+import java.util.ArrayList;
+
 public class MeineArrayList<E> {
 
     private E[] list;
@@ -14,4 +16,43 @@ public class MeineArrayList<E> {
         size = 0;
         increment = inc;
     }
+
+    public Object[] newList(){
+        Object[] liste = new Object[0];
+        return liste;
+    }
+
+    public static Object[] addToList(Object[] liste, Object neuesElement){
+        liste[liste.length +1] = neuesElement;
+        return liste;
+    }
+
+    public Object[] delete(int what, Object[] liste) {
+        liste[what] = null;
+        return liste;
+    }
+
+    public Object[] deleteWholeList(Object[] liste){
+        liste = new Object[0];
+        return liste;
+    }
+
+    public void readAll(Object[] liste) {
+        int x = 0;
+        while (x < liste.length) {
+            String[] neuesArray = (String[]) liste[x];
+            System.out.print(neuesArray[0]);
+            System.out.println(neuesArray[1]);
+            x++;
+        }
+    }
+
+    public int getSize(MeineArrayList<E> liste){
+        return liste.size;
+    }
+
+
+
+
+
 }
