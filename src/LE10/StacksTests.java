5eafa05a4eecf4b6702d25eec5427db31e709736
alexhurtig <alexander.hurtig@friendsurance.de>
2017-12-18 22:27:58 +0100

    LE10 nachbearbeitet

--- src/LE10/StacksTests.java
+++ src/LE10/StacksTests.java
@@ -1,162 +1,141 @@
 package LE10;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StacksTests {
 
-    //Schreiben Sie eine Klasse namens StacksTests.java, welche die die Klassen Stack<E>, ArrayStack<E>, ListStack<E> und die Klasse java.util.Stack<E>
-    // ausprobiert. Erzeugen Sie dabei verschieden Objekte mit mindestens zwei verschiedenen Werten für den generischen Parameter: String und ein
-    // Parameter Ihrer Wahl.
+  //Schreiben Sie eine Klasse namens StacksTests.java, welche die die Klassen Stack<E>, ArrayStack<E>, ListStack<E> und die Klasse java.util.Stack<E>
+  // ausprobiert. Erzeugen Sie dabei verschieden Objekte mit mindestens zwei verschiedenen Werten für den generischen Parameter: String und ein
+  // Parameter Ihrer Wahl.
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Stack<ArrayList[]> as = new ArrayStack<>(5);
+    Stack<String> as = new ArrayStack<>(5);
 
-        ArrayList[] insert = new ArrayList[1];
+    try {
+      System.out.println(as);
 
-        ArrayList<String> als = new ArrayList<>();
+      as.push("dhgfjkasl");
+      as.push("ghasdfjkl");
+      as.push("tqireq");
+      as.push("hjvlfab");
+      as.push("dfa");
 
-        try {
-            System.out.println(as);
-            insert[0] = als;
-            als.add("dhgfjkasl");
-            as.push(insert);
-            as.push(insert);
-            as.push(insert);
-            as.push(insert);
-            as.push(insert);
+      System.out.println(as);
 
-            System.out.println(as);
+      System.out.println("as.pop " + as.pop());
+      System.out.println("as.pop " + as.pop());
+      System.out.println("as.pop " + as.pop());
 
-            System.out.println("as.pop " + as.pop());
-            System.out.println("as.pop " + as.pop());
-            System.out.println("as.pop " + as.pop());
-            System.out.println(as);
+      System.out.println(as);
 
+      as.push("hjvlfab");
+      as.push("dfa");
+      as.push("fdasfa");
 
-            as.push(insert);
-            as.push(insert);
-            as.push(insert);
-            System.out.println(as);
+      System.out.println(as);
 
-            as.push(insert);
-        } catch (FullStackException | EmptyStackException e) {
-            System.out.println(e.getMessage());
-        }
+      as.push("req");
 
-        System.out.println("----------------------------------------------------------");
-
-        Stack<ArrayList[]> as2 = new ArrayStack<>(5);
-        ArrayList<Integer> ali = new ArrayList<>();
-
-        try {
-            System.out.println(as);
-            insert[0] = ali;
-            ali.add(123);
-            as2.push(insert);
-            as2.push(insert);
-            as2.push(insert);
-            as2.push(insert);
-            as2.push(insert);
-
-            System.out.println(as2);
-
-            System.out.println("as2.pop " + as2.pop());
-            System.out.println("as2.pop " + as2.pop());
-            System.out.println("as2.pop " + as2.pop());
-            System.out.println(as2);
-
-
-            as2.push(insert);
-            as2.push(insert);
-            as2.push(insert);
-            System.out.println(as2);
-
-            as2.push(insert);
-        } catch (FullStackException | EmptyStackException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("----------------------------------------------------------");
-
-        Stack<List[]> ls = new ListStack<>();
-
-        List[] l1 = new List[5];
-        List[] l2 = new List[5];
-        List[] l3 = new List[5];
-
-        List<String> ls1 = new ArrayList<>(2);
-        l1[0] = ls1;
-        l2[0] = ls1;
-        l3[0] = ls1;
-
-        try {
-            System.out.println(ls);
-            ls.push(l1);
-            ls.push(l1);
-            ls.push(l2);
-            ls.push(l3);
-            ls.push(l3);
-
-            System.out.println(ls);
-
-            System.out.println("ls.pop " + ls.pop());
-            System.out.println("ls.pop " + ls.pop());
-            System.out.println("ls.pop " + ls.pop());
-            System.out.println(ls);
-
-
-            ls.push(l3);
-            ls.push(l2);
-            ls.push(l1);
-            System.out.println(ls);
-
-            ls.push(l2);
-
-        } catch (FullStackException | EmptyStackException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("----------------------------------------------------------");
-
-        Stack<List[]> ls2 = new ListStack<>();
-
-        List[] li1 = new List[5];
-        List[] li2 = new List[5];
-        List[] li3 = new List[5];
-
-        List<Integer> liI1 = new ArrayList<>(2);
-
-        l1[0] = liI1;
-        l2[0] = liI1;
-        l3[0] = liI1;
-
-        try {
-            System.out.println(ls2);
-            ls2.push(li1);
-            ls2.push(li1);
-            ls2.push(li2);
-            ls2.push(li3);
-            ls2.push(li3);
-
-            System.out.println(ls2);
-
-            System.out.println("ls2.pop " + ls2.pop());
-            System.out.println("ls2.pop " + ls2.pop());
-            System.out.println("ls2.pop " + ls2.pop());
-            System.out.println(ls2);
-
-
-            ls2.push(li3);
-            ls2.push(li2);
-            ls2.push(li1);
-            System.out.println(ls2);
-
-            ls2.push(li2);
-
-        } catch (FullStackException | EmptyStackException e) {
-            System.out.println(e.getMessage());
-        }
+    } catch (FullStackException | EmptyStackException e) {
+      System.out.println(e.getMessage());
     }
+
+    System.out.println("----------------------------------------------------------");
+
+    Stack<Integer> as2 = new ArrayStack<>(5);
+
+    try {
+      System.out.println(as);
+
+      as2.push(123);
+      as2.push(234);
+      as2.push(345);
+      as2.push(456);
+      as2.push(567);
+
+      System.out.println(as2);
+
+      System.out.println("as2.pop " + as2.pop());
+      System.out.println("as2.pop " + as2.pop());
+      System.out.println("as2.pop " + as2.pop());
+
+      System.out.println(as2);
+
+      as2.push(567);
+      as2.push(789);
+      as2.push(678);
+
+      System.out.println(as2);
+
+      as2.push(890);
+
+    } catch (FullStackException | EmptyStackException e) {
+      System.out.println(e.getMessage());
+    }
+
+    System.out.println("----------------------------------------------------------");
+
+    Stack<String> ls = new ListStack<>();
+
+    try {
+      System.out.println(ls);
+
+      ls.push("asdfd");
+      ls.push("fgsdhja");
+      ls.push("hgfd");
+      ls.push("gfsf");
+      ls.push("lkhklh");
+
+      System.out.println(ls);
+
+      System.out.println("ls.pop " + ls.pop());
+      System.out.println("ls.pop " + ls.pop());
+      System.out.println("ls.pop " + ls.pop());
+
+      System.out.println(ls);
+
+      ls.push("tretwe");
+      ls.push("bvcxb");
+      ls.push("gfhtd");
+
+      System.out.println(ls);
+
+      ls.push("nhdz");
+
+    } catch (FullStackException | EmptyStackException e) {
+      System.out.println(e.getMessage());
+    }
+
+    System.out.println("----------------------------------------------------------");
+
+    Stack<Integer> ls2 = new ListStack<>();
+
+    try {
+      System.out.println(ls2);
+
+      ls2.push(123);
+      ls2.push(234);
+      ls2.push(345);
+      ls2.push(654);
+      ls2.push(765);
+
+      System.out.println(ls2);
+
+      System.out.println("ls2.pop " + ls2.pop());
+      System.out.println("ls2.pop " + ls2.pop());
+      System.out.println("ls2.pop " + ls2.pop());
+
+      System.out.println(ls2);
+
+      ls2.push(876);
+      ls2.push(987);
+      ls2.push(998);
+
+      System.out.println(ls2);
+
+      ls2.push(150);
+
+    } catch (FullStackException | EmptyStackException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
